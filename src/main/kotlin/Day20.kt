a8937a7f27115f8815534f1ad316546d40167529
Olaf Gottschalk <Olaf.Gottschalk@bmwgroup.com>
2023-12-20 17:59:39 +0100

    Refactor day 20 and include GraphViz

--- src/main/kotlin/Day20.kt
+++ src/main/kotlin/Day20.kt
@@ -1,6 +1,8 @@
 import Day20.Module.*
 import guru.nidi.graphviz.attribute.Color
 import guru.nidi.graphviz.attribute.Label
+import guru.nidi.graphviz.attribute.Rank.sepEqually
+import guru.nidi.graphviz.attribute.Style
 import guru.nidi.graphviz.engine.Format
 import guru.nidi.graphviz.graph
 import guru.nidi.graphviz.toGraphviz
@@ -140,37 +142,45 @@ class Day20 : Day(20, 2023, "Pulse Propagation") {
         return highCount.toLong() * lowCount
     }
 
-    fun createGraph(modules: Map<String, Module>) {
-        val clusters = modules.values.filter { m -> m.inputs.size > 4 }
+    fun createGraph(modules: Map<String, Module>, clusters: List<Cluster>) {
 
         fun Module.color(): Color = when (this) {
             is Conjunction -> Color.RED
             is FlipFlop -> Color.BLUE
-            else -> Color.BLACK
+            else -> Color.GREEN
         }
 
         graph(directed = true) {
-            "broadcaster"[Color.GREEN]
-
-            clusters.forEach { center ->
-                graph(name = center.name, directed = true, cluster = true) {
-                    graph[Label.lines(center.name)]
-                    center.name[Color.RED]
-                    val flipFlops = (center.inputs + center.outputs).filterIsInstance<FlipFlop>()
+            clusters.forEach { cluster ->
+                graph(name = cluster.name, directed = true, cluster = true) {
+                    graph[Label.lines("${cluster.name}: ${cluster.bits}")]
+                    cluster.center.name[Color.RED]
                     graph(directed = true, cluster = true) {
                         graph[Label.lines("FFs")]
-                        flipFlops.forEach {
-                            it.name[it.color()]
+                        cluster.flipFlops.forEach {
+                            val ff = it.name[it.color()]
+                            if (cluster.center in it.outputs) {
+                                ff[Style.lineWidth(3.0)]
+                            }
                         }
                     }
                 }
             }
 
+            val r = clusters.map { it.cycleLength() }.lcm()
+
             graph(name = "result", directed = true, cluster = true) {
+                graph[Label.lines("Result cluster"), sepEqually(10.0)]
                 modules.values.filter {
                     it is Conjunction && (it.inputs.size <= 4)
                 }.forEach { x ->
-                    x.name[Color.RED]
+                    val cluster = clusters.singleOrNull { it.output == x }
+                    val n = x.name[Color.RED]
+                    if (cluster != null) {
+                        n[Label.lines(x.name, "cycle: ${cluster.cycleLength()}")]
+                    } else {
+                        n[Label.lines(x.name, "cycle: $r")]
+                    }
                     x.inputs.forEach {
                         it.name[it.color()]
                     }
@@ -190,13 +200,21 @@ class Day20 : Day(20, 2023, "Pulse Propagation") {
 
     data class Cluster(
         val center: Conjunction,
-        val flipFlops: List<FlipFlop>,
         val input: FlipFlop,
         val output: Module
     ) {
-        override fun toString(): String {
-            return "Counter ${center.name} with ${flipFlops.size} FFs - input is ${input.name}, output is ${output.name}"
+        val name: String get() = center.name
+        val flipFlops =
+            generateSequence(input) { it.outputs.filterIsInstance<FlipFlop>().singleOrNull() }.toList()
+        val bits = flipFlops.reversed().joinToString("") {
+            if (it.outputs.any { it == center }) "1" else "0"
         }
+
+        override fun toString(): String {
+            return "Cluster ${center.name} with ${flipFlops.size} FFs - input is ${input.name}, output is ${output.name}"
+        }
+
+        fun cycleLength() = bits.toInt(2)
     }
 
     override fun part2(): Long {
@@ -205,36 +223,38 @@ class Day20 : Day(20, 2023, "Pulse Propagation") {
 
         val clusters = modules.values.filterIsInstance<Conjunction>().filter { m -> m.inputs.size > 4 }
             .map { center ->
-                val ff = modules.values.filterIsInstance<FlipFlop>()
+                val attached = modules.values.filterIsInstance<FlipFlop>()
                     .filter { it in center.inputs || it in center.outputs }
-                val all = ff + center
-                val input = ff.single { it.inputs.any { it !in all } }
-                val output = all.flatMap { it.outputs }.filterNotNull().single { it !in all }
-                Cluster(center, ff, input, output)
+                val all = attached + center
+                val input = attached.single { it.inputs.any { it !in all } }
+                val output = center.outputs.filterNotNull().single { it !in all }
+                Cluster(center, input, output)
             }
 
-        val cycles = clusters.map {
-            alog { it }
+//        createGraph(modules, clusters)
 
-            var count = 0
-            outer@ while (true) {
-                count++
-                val q = dequeOf<Pulse>(Pulse.Low(start, it.input))
-                while (q.isNotEmpty()) {
-                    val p = q.removeFirst()
-                    if (p is Pulse.Low && p.to == it.output)
-                        break@outer
+//        val cycles = clusters.map {
+//            alog { it }
+//
+//            var count = 0
+//            outer@ while (true) {
+//                count++
+//                val q = dequeOf<Pulse>(Pulse.Low(start, it.input))
+//                while (q.isNotEmpty()) {
+//                    val p = q.removeFirst()
+//                    if (p is Pulse.Low && p.to == it.output)
+//                        break@outer
+//
+//                    p.to?.let {
+//                        q.addAll(it.receive(p))
+//                    }
+//                }
+//            }
+//            alog { "Cycles after $count (${it.cycleLength()}) button presses!" }
+//            count
+//        }
 
-                    p.to?.let {
-                        q.addAll(it.receive(p))
-                    }
-                }
-            }
-            alog { "Cycles after $count button presses!" }
-            count
-        }
-
-        return cycles.lcm()
+        return clusters.map { it.cycleLength() }.lcm()
     }
 
 }
